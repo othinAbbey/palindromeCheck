e9b91115f9264d55f964409936ebe6b97a200f3e
onyango abbey <117769358+othinAbbey@users.noreply.github.com>
2023-08-14 09:24:32 +0300

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+let words = ["kayak", "noon", "hi", "hello", "madam"];
+function firstPalindrome(words) {
+  let palindrome = [];
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let reverseWord = word.split("").reverse().join("");
+    if (word === reverseWord) {
+      palindrome.push(word);
+      console.log(`${palindrome[0]} is a palindrome`);
+      break;
+    } else {
+      console.log(`${word} is not a palindrome`);
+    }
+  }
+  return;
+}
+firstPalindrome(words);
